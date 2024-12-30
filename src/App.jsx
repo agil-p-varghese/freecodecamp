93c9683819e5ca1dfc914c5ecb4b4189b82d501a
agil-p-varghese <agilpvargheese0008@gmail.com>
2024-12-30 19:45:57 +0530

    Initial commit

---
+++ src/App.jsx
@@ -0,0 +1,42 @@
+import React, { useState, useEffect } from "react";
+import "./App.css";
+
+const App = () => {
+  const [quote, setQuote] = useState("");
+  const [author, setAuthor] = useState("");
+
+  // Function to fetch a random quote
+  const fetchQuote = async () => {
+    const response = await fetch("https://api.quotable.io/random");
+    const data = await response.json();
+    setQuote(data.content);
+    setAuthor(data.author);
+  };
+
+  // Fetch a quote when the app loads
+  useEffect(() => {
+    fetchQuote();
+  }, []);
+
+  return (
+    <div id="quote-box" className="wrapper">
+      <div id="text">"{quote}"</div>
+      <div id="author">- {author}</div>
+      <button id="new-quote" onClick={fetchQuote}>
+        New Quote
+      </button>
+      <a
+        id="tweet-quote"
+        href={`https://twitter.com/intent/tweet?text=${encodeURIComponent(
+          `"${quote}" - ${author}`
+        )}`}
+        target="_blank"
+        rel="noopener noreferrer"
+      >
+        Tweet
+      </a>
+    </div>
+  );
+};
+
+export default App;
