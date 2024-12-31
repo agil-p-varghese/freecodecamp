6968ad3388a921ce50b9b6fd501cdfeadb7a3053
agil-p-varghese <agilpvargheese0008@gmail.com>
2024-12-31 23:14:46 +0530

    all deleted

--- vite.config.js
+++
@@ -1,7 +0,0 @@
-import { defineConfig } from 'vite';
-import react from '@vitejs/plugin-react';
-
-export default defineConfig({
-  base: '/freecodecamp/', // Add the repository name as the base
-  plugins: [react()],
-});
