b3be34523315d61bd74168b8ac2ff51e9c97b2d7
agil-p-varghese <agilpvargheese0008@gmail.com>
2025-01-01 22:47:31 +0530

    commit1

--- project1/src/App.jsx
+++ project1/src/App.jsx
@@ -1,35 +1,39 @@
-import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
-import './App.css'
+import React, { useState } from "react";
+import "./App.css";
 
-function App() {
-  const [count, setCount] = useState(0)
+const quotesList = [
+  { quote: "I'm not sure if I was the first man in space or the last dog.", source: "- Yuri Gagarin", year: "1961", tags: "Humor" },
+  { quote: "'We’ll never survive!' 'Nonsense. You’re only saying that because no one ever has.'", source: "- William Goldman", citation: "The Princess Bride", year: "1987", tags: "Inspirational" },
+  { quote: "You know, Hobbes, some days even my lucky rocket ship underpants don't help.", source: "- Bill Watterson", citation: "Calvin and Hobbes", tags: "Humor" },
+  { quote: "Yet man will never be perfect until he learns to create and destroy; he does know how to destroy, and that is half the battle.", source: "- Alexandre Dumas", citation: "The Count of Monte Cristo", year: "1844", tags: "Humanity" },
+  // Add more quotes here
+];
+
+const App = () => {
+  const [quoteIndex, setQuoteIndex] = useState(Math.floor(Math.random() * quotesList.length));
+
+  // Function to generate a new random quote
+  const getRandomQuote = () => {
+    const newIndex = Math.floor(Math.random() * quotesList.length);
+    setQuoteIndex(newIndex);
+  };
+
+  const currentQuote = quotesList[quoteIndex];
 
   return (
-    <>
-      <div>
-        <a href="https://vite.dev" target="_blank">
-          <img src={viteLogo} className="logo" alt="Vite logo" />
-        </a>
-        <a href="https://react.dev" target="_blank">
-          <img src={reactLogo} className="logo react" alt="React logo" />
-        </a>
+    <div id="quote-box" className="wrapper">
+      <div id="text">"{currentQuote.quote}"</div>
+      <div id="author">
+        {currentQuote.source}
+        {currentQuote.year && `, ${currentQuote.year}`}
       </div>
-      <h1>Vite + React</h1>
-      <div className="card">
-        <button onClick={() => setCount((count) => count + 1)}>
-          count is {count}
-        </button>
-        <p>
-          Edit <code>src/App.jsx</code> and save to test HMR
-        </p>
-      </div>
-      <p className="read-the-docs">
-        Click on the Vite and React logos to learn more
-      </p>
-    </>
-  )
-}
+      <button id="new-quote" onClick={getRandomQuote}>
+        New Quote
+      </button>
+      <a id="tweet-quote" href="twitter.com/intent/tweet">tweet</a>
+      
+    </div>
+  );
+};
 
-export default App
+export default App;
